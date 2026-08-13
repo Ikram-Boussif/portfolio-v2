51288a2771ddc5d1c0ee70ffa2c6306f767adf26
Ikram-Boussif <ikram.boussif.tn@gmail.com>
2026-08-13 22:21:06 +0100

    fix: add environment file for Vercel build

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  emailjsServiceId: 'service_ob11jyj',    // Service ID
+  emailjsTemplateId: 'template_q8jwwd5',  // Template ID
+  emailjsPublicKey: 'LShQcDBUv6omE58OS'     // Public Key
+};
